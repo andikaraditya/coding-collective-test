dad6d1207e71cfcf9131845535fd82d0aa0dce3d
andikaraditya <andikaraditya1@gmail.com>
2024-12-13 06:43:15 +0700

    default active user

--- database/migrations/2024_12_12_231635_add_user_status.php
+++ database/migrations/2024_12_12_231635_add_user_status.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('status')->nullable()->default("Inactive");
+            $table->string('status')->nullable()->default("Active");
         });
     }
 
